e00a8fbb872155bb713fb77485de2e81c1170274
jonny5 <jonwiththewind@gmail.com>
2019-06-21 15:57:56 -0700

    Add check for if state is passed before sending to API

--- api_client.py
+++ api_client.py
@@ -3,7 +3,11 @@ import requests
 
 API_KEY = "X35RriQxgbDSeCGrBTNoOPnbwTbV4FYsKyz2YgH8"
 BASE_URL = "https://developer.nps.gov/api/v1/"
+
 class ApiClient:
     def parks(state):
-        response = requests.get(BASE_URL + "parks", params={'api_key':API_KEY, 'limit':'10', 'stateCode': state})
+        params = {'api_key': API_KEY, 'limit': 10}
+        if state:
+            params['stateCode'] = state
+        response = requests.get(BASE_URL + "parks", params=params)
         return response.json()['data']
