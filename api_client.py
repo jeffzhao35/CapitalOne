43ef6bad20f7f5abeb843042df19c915402bb9ab
Jeff <jeffzhao35@gmail.com>
2019-06-22 11:04:36 -0700

    modified results table

--- api_client.py
+++ api_client.py
@@ -4,6 +4,6 @@ import requests
 API_KEY = "X35RriQxgbDSeCGrBTNoOPnbwTbV4FYsKyz2YgH8"
 BASE_URL = "https://developer.nps.gov/api/v1/"
 class ApiClient:
-    def parks(state):
-        response = requests.get(BASE_URL + "parks", params={'api_key':API_KEY, 'limit':'10', 'stateCode': state})
+    def parks(park_name, state, designation, keyword):
+        response = requests.get(BASE_URL + "parks", params={'api_key':API_KEY, 'limit':'10', 'parkCode': park_name, 'stateCode': state, 'q': designation, 'q': keyword})
         return response.json()['data']
--- flaskapp.py
+++ flaskapp.py
@@ -6,14 +6,24 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	if request.method == 'POST':
+		park_name = request.form['park_name']
+		print(park_name)
 		state = request.form['state']
-		return redirect(url_for('results', state=state))
+		designation = request.form['designation']
+		keyword = request.form['keyword']
+		return redirect(url_for('results', park_name=park_name, state=state, designation=designation, keyword=keyword))
 	else:
 		return render_template("index.html")
 
 
 @app.route('/results', methods=['GET'])
 def results():
+	park_name = request.args.get('park_name')
 	state = request.args.get('state')
-	parks = ApiClient.parks(state=state)
+	designation = request.args.get('designation')
+	keyword = request.args.get('keyword')
+	parks = ApiClient.parks(park_name=park_name, state=state, designation=designation, keyword=keyword)
 	return render_template("results.html", parks=parks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
