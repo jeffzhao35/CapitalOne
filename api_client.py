51333fab85f78c61b13e9dad8897f73620e56f67
Jeff <jeffzhao35@gmail.com>
2019-06-22 13:05:29 -0700

    updated scrollable table and links to specific parks

--- api_client.py
+++ api_client.py
@@ -5,5 +5,5 @@ API_KEY = "X35RriQxgbDSeCGrBTNoOPnbwTbV4FYsKyz2YgH8"
 BASE_URL = "https://developer.nps.gov/api/v1/"
 class ApiClient:
     def parks(park_name, state, designation, keyword):
-        response = requests.get(BASE_URL + "parks", params={'api_key':API_KEY, 'limit':'10', 'parkCode': park_name, 'stateCode': state, 'q': designation, 'q': keyword})
+        response = requests.get(BASE_URL + "parks", params={'api_key':API_KEY, 'limit':'50', 'parkCode': park_name, 'stateCode': state, 'q': designation, 'q': keyword})
         return response.json()['data']
