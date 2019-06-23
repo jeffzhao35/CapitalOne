71be940240f64f65074d02dd74315e42b2c4a8ce
Jeff <jeffzhao35@gmail.com>
2019-06-23 10:13:23 -0700

    modified table

--- api_client.py
+++ api_client.py
@@ -5,12 +5,15 @@ API_KEY = "X35RriQxgbDSeCGrBTNoOPnbwTbV4FYsKyz2YgH8"
 BASE_URL = "https://developer.nps.gov/api/v1/"
 
 class ApiClient:
-    def parks(park_name, state, designation, keyword):
-        params={'api_key':API_KEY, 'limit':'50', 'parkCode': park_name, 'stateCode': state, 'q': designation, 'q2': keyword}
-        if state:
-        	params['parkCode'] = park_name
-            params['stateCode'] = state
-            params['q'] = designation
-            params['q2'] = keyword
-        response = requests.get(BASE_URL + "parks", params=params)
-        return response.json()['data']
+	def parks(park_name, state, designation, keyword):
+		params={'api_key':API_KEY, 'limit':'50'}
+		if park_name:
+			params['parkCode'] = park_name
+		if state:
+			params['stateCode'] = state
+		if designation:
+			params['q'] = designation
+		if keyword:
+			params['q'] = keyword
+		response = requests.get(BASE_URL + "parks", params=params)
+		return response.json()['data']
